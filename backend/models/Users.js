6f2681e6062dfc3cfe71887741bf483d27c3da39
Neha <nehaputhran3108@gmail.com>
2024-06-08 19:41:55 +0530

    user schema

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -2,10 +2,22 @@ const mongoose = require('mongoose');
 
 
 const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    dept: String,
+    name: {
+        type: String,
+        required: true,
+      },
+    email: {
+        type: String,
+        required: true,
+      },
+    password: {
+        type: String,
+        required: true,
+      },
+    dept:  {
+        type: String,
+        required: true,
+      },
 });
 
 
