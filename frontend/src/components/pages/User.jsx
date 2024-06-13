2b9b6c8a8b7f4d17d5b84f1faad395bddc60ff57
NishalNT <nishalnthingalaya1@gmail.com>
2024-06-13 15:14:12 +0530

    user page

--- frontend/src/components/pages/User.jsx
+++ frontend/src/components/pages/User.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import Header from "../Header";
 import UserSidebar from '../UserSidebar';
+import MainContent from '../MainContent';
+import Footer from '../Footer';
+import { Toaster } from 'react-hot-toast';
 function User() {
   return (
-    <div>
-       <Header
-        // toggleTheme={toggleTheme}
-        // isDarkTheme={isDarkTheme}
-        // themeClass={themeClass}
-        // transitionClass={transitionClass}
-        // buttonThemeClass={buttonThemeClass}
-      />
+    <div className='flex flex-col h-screen'>
+       <Header/>
+      <div className="flex flex-1">
       <UserSidebar />
-      
+        {/* <SideBar themeClass={themeClass} transitionClass={transitionClass} /> */}
+        <div className="flex flex-col flex-1">
+          <MainContent />
+          <Footer />
+          <Toaster />
+        </div>
+      </div>
     </div>
   )
 }
